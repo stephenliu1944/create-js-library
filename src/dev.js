111833daebf791544806013993bd70d5454b14f9
liujunjie <liujunjie@bbdservice.com>
2018-12-09 16:17:26 +0800

    ..

--- src/dev.js
+++ src/dev.js
@@ -6,6 +6,7 @@ import { module1, module2 } from './index';
 var person = module1();
 var age = module2(person.age);
 function a(){
-    console.log('aaaaa');
+    var a = {t1:1,t2:2};
+    console.log('aaaaa',a);
 }
 document.querySelector('#app').innerHTML = `<h1>hello ${person.name}, ${age}!</h1><h2>DEV: ${__DEV__}</h2>`;
